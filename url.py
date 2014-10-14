06b288f6869336929869c3d066a931bad1590dc7
yyf1986 <yyf1986@users.noreply.github.com>
2014-10-14 15:20:32 +0800

    Create url.py

---
+++ url.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python2.6
+#-*- coding: utf-8 -*-
+'''
+Created on 2014-2-25
+
+@author: YAO
+
+'''
+
+import socket
+import httplib
+
+def checkport(ip,port):
+    ''' 检查端口，超时时间2s，通过返回值来判断端口是否开启  o是正常，N-connect端口不能连接'''
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2)
+        s.connect((ip,port))
+        s.close()
+        restlt = "o"
+    except Exception:
+        restlt = "N-connect"
+    return restlt
+def getcode(ip,port,url,timeout=5):
+    ''' 抓取url状态码，返回结果为字符串类型，异常为N-connect '''
+    try:
+        conns = httplib.HTTPConnection(ip,port,True,timeout)
+        if not url.startswith('/'):
+            url="/"+url
+        conns.request("GET",url)
+        response = conns.getresponse()
+        code = response.status
+        result = code
+    except Exception:
+        result = "N-connect"
+    finally:
+        conns.close()
+    return result
