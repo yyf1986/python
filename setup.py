67b9b80edd641caf1baa8e8d70363c932bc24b80
YAO <yyf1986@users.noreply.github.com>
2015-05-13 14:46:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 '''
 File Name: setup.py
 Author: Yao
-mail: 11113072@cnsuning.com
 Created Time: Sun 18 May 2014 05:58:44 PM CST
 '''
 from distutils.core import setup
@@ -12,6 +11,6 @@ setup(
 		version='2.0',
 		description='sn base script',
 		author='yao',
-		author_email='11113072@cnsuning.com',
+		author_email='123@123.com',
 		packages = ["snbase"]
 		)
